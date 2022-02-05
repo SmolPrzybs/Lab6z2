d14aad8f17c620e09be3f0bfbcec90232cefb1d8
Maciej Przybyła <95914501+SmolPrzybs@users.noreply.github.com>
2022-02-06 00:00:40 +0100

    Add files via upload

---
+++ Lab6z2/Lab6z2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int tab[9];
+
+	cout << tab << endl;
+	cout << tab + 3 << endl;
+
+	srand(time(NULL));
+	for (int i = 0; i < 9; i++)
+	{
+		*(tab + i) = rand() % 10;
+		cout << *(tab + i) << " ";
+		cout << tab + i << endl;
+	}
+}
+
